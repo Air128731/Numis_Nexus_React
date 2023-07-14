224a927c5d606fe3e7bc033f388539061304b20a
Air128731 <henry.scott0x8080@gmail.com>
2023-07-13 20:13:38 -0400

    build faster removed

--- src/components/Landing/Feature.js
+++ src/components/Landing/Feature.js
@@ -47,13 +47,13 @@ export default function Example() {
         <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">Build faster</h2>
+              {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">Build faster</h2> */}
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our Goals</p>
-              <dl className="max-w-xl mt-10 space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+              <dl className="max-w-xl mt-10 space-y-4 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold text-gray-900">
-                      <feature.icon className="absolute w-5 h-5 text-indigo-600 left-1 top-1" aria-hidden="true" />
+                      {/* <feature.icon className="absolute w-5 h-5 text-indigo-600 left-1 top-1" aria-hidden="true" /> */}
                       {/* {feature.name} */}
                     </dt>{' '}
                     <dd className="inline">{feature.description}</dd>
